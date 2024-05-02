53ed2df34ed5e3f883955a917c5312acee7a8406
splasencia <sergio.spa@gmail.com>
2024-05-02 22:43:23 +0200

    misc updates

--- server.js
+++ server.js
@@ -32,11 +32,5 @@ app.get('/links', async (req, res) => {
     res.json(links);
 });
 
-function sum(a, b) {
-  return a + b;
-}
-
-module.exports = sum;
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
